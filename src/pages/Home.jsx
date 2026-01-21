3d960095668ff47b09e70621bd39628e433758ba
Cristian Gobbesso <cristiangobbesso.it@gmail.com>
2026-01-20 22:23:33 -0300

    Agregar búsqueda y eliminación de usuarios

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
 import UserList from "../components/UserList"
 
 
 const Home = ()=>{
+    const [search,setSearch] = useState("")
     return (
         <div>
             <h1>Gestion de Usuarios</h1>
-            <UserList/>
+            <input type="texte"
+            placeholder="Buscar usuario..."
+            value={search}
+            onChange={(e)=>setSearch(e.target.value)}
+            ></input>
+            <UserList search={search} />
         </div>
     );
 };
