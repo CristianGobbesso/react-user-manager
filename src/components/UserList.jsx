839eb0006b67f0db5d2449f4f004019d6670087c
Cristian Gobbesso <cristiangobbesso.it@gmail.com>
2026-01-20 22:20:51 -0300

    Agregar servicio de usuarios y fetch con useEffect

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -2,10 +2,12 @@ import { useEffect, useState } from "react"
 import { getUsers } from "../services/userService";
 import UserItem from "./UserItem";
 
-const UserList = ()=> {
+const UserList = ({search})=> {
     const [users, setusers] = useState([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
+
+    
 
     useEffect(()=>{
         const fetchUsers = async ()=>{
@@ -33,6 +35,10 @@ const UserList = ()=> {
         );
     };
 
+    const filteredUsers = users.filter(user => 
+        user.name.toLowerCase().includes(search.toLowerCase()) ||
+        user.email.toLowerCase().includes(search.toLowerCase())
+    );
 
 if(loading) return <p>Cargando usuarios...⌛</p>;
 if(error) return <p>{error}</p>;
@@ -40,13 +46,17 @@ if(error) return <p>{error}</p>;
 
 return (
     <ul>
-        {users.map((user)=>(
+        {filteredUsers.length === 0 ?(
+            <li>No hay usuarios para mostrar</li>
+        ) : (
+        filteredUsers.map((user)=>(
             <UserItem 
             key={user.id}
             user= {user}
             onDelete={handleDeleteuser} 
             />
-        ))}
+        ))
+    )}
     </ul>
 );
 };
