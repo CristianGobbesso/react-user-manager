963dc086c56fbcc6ac3e6c03ba1134fe9cae67cf
Cristian Gobbesso <cristiangobbesso.it@gmail.com>
2026-01-21 22:04:55 -0300

    Refactorizar UserList como componente presentacional

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,62 +1,24 @@
-import { useEffect, useState } from "react"
-import { getUsers } from "../services/userService";
+
 import UserItem from "./UserItem";
 
-const UserList = ({search})=> {
-    const [users, setusers] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
+const UserList = ({ users = [],  onDelete }) => {
+  
 
-    
+  if (users.length === 0) {
+    return <p>No hay usuarios para mostrar</p>;
+  }
 
-    useEffect(()=>{
-        const fetchUsers = async ()=>{
-            try{
-                setLoading(true);
-                setError(null);
-
-                const data = await getUsers()
-                setusers(data);
-            }
-            catch (error) {
-                setError(error.message);
-            }
-            finally{
-                setLoading(false);
-            }
-
-        };
-        fetchUsers();
-    },[]);
-
-    const handleDeleteuser = (id) => {
-        setusers((prevusers)=>
-        prevusers.filter((user)=>user.id !==id)
-        );
-    };
-
-    const filteredUsers = users.filter(user => 
-        user.name.toLowerCase().includes(search.toLowerCase()) ||
-        user.email.toLowerCase().includes(search.toLowerCase())
-    );
-
-if(loading) return <p>Cargando usuarios...⌛</p>;
-if(error) return <p>{error}</p>;
 
 
 return (
     <ul>
-        {filteredUsers.length === 0 ?(
-            <li>No hay usuarios para mostrar</li>
-        ) : (
-        filteredUsers.map((user)=>(
-            <UserItem 
-            key={user.id}
-            user= {user}
-            onDelete={handleDeleteuser} 
-            />
-        ))
-    )}
+      {users.map((user) => (
+        <UserItem
+          key={user.id}
+          user={user}
+          onDelete={onDelete}
+        />
+      ))}
     </ul>
 );
 };
