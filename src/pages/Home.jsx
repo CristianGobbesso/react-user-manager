4c1648239c87d4068507389eecaa206d6697008a
Cristian Gobbesso <cristiangobbesso.it@gmail.com>
2026-01-21 22:06:00 -0300

    Mover lógica de usuarios y búsqueda al componente Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,68 @@
-import { useState } from "react";
-import UserList from "../components/UserList"
+import { useEffect, useState } from "react";
+import SearchInput from "../components/SearchInput";
+import UserList from "../components/UserList";
+import { getUsers } from "../services/userService";
 
 
 const Home = ()=>{
+    const [users, setusers] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
     const [search,setSearch] = useState("")
+
+    useEffect(()=>{
+            const fetchUsers = async ()=>{
+                try{
+                    setLoading(true);
+                    setError(null);
+    
+                    const data = await getUsers()
+                    setusers(data);
+                }
+                catch (error) {
+                    setError(error.message);
+                }
+                finally{
+                    setLoading(false);
+                }
+    
+            };
+            fetchUsers();
+        },[]);
+    
+        const handleDeleteuser = (id) => {
+            setusers((prevusers)=>
+            prevusers.filter((user)=>user.id !==id)
+            );
+        };
+    
+        const filteredUsers = users.filter(user => 
+            user.name.toLowerCase().includes(search.toLowerCase()) ||
+            user.email.toLowerCase().includes(search.toLowerCase())
+        );
+
+        if (loading) return <p>Cargando usuarios... ⌛</p>;
+  if (error) return <p>{error}</p>;
+
     return (
-        <div>
-            <h1>Gestion de Usuarios</h1>
-            <input type="texte"
-            placeholder="Buscar usuario..."
-            value={search}
-            onChange={(e)=>setSearch(e.target.value)}
-            ></input>
-            <UserList search={search} />
-        </div>
-    );
+    <>
+      <h1>Gestión de Usuarios</h1>
+
+      <SearchInput
+        value={search}
+        setSearch={setSearch}
+      />
+
+      {filteredUsers.length === 0 ? (
+        <p>No hay usuarios para mostrar</p>
+      ) : (
+        <UserList
+          users={filteredUsers}
+          onDelete={handleDeleteuser}
+        />
+      )}
+    </>
+  );
 };
 
 export default Home;
