f8bd5686ecca10fdfe2f7e6fddefb430767f59ce
Cristian Gobbesso <cristiangobbesso.it@gmail.com>
2026-01-21 22:20:17 -0300

    feat: input de búsqueda controlado y botón para limpiar búsqueda

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,54 +4,62 @@ import UserList from "../components/UserList";
 import { getUsers } from "../services/userService";
 
 
-const Home = ()=>{
-    const [users, setusers] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
-    const [search,setSearch] = useState("")
+const Home = () => {
+  const [users, setusers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [search, setSearch] = useState("")
 
-    useEffect(()=>{
-            const fetchUsers = async ()=>{
-                try{
-                    setLoading(true);
-                    setError(null);
-    
-                    const data = await getUsers()
-                    setusers(data);
-                }
-                catch (error) {
-                    setError(error.message);
-                }
-                finally{
-                    setLoading(false);
-                }
-    
-            };
-            fetchUsers();
-        },[]);
-    
-        const handleDeleteuser = (id) => {
-            setusers((prevusers)=>
-            prevusers.filter((user)=>user.id !==id)
-            );
-        };
-    
-        const filteredUsers = users.filter(user => 
-            user.name.toLowerCase().includes(search.toLowerCase()) ||
-            user.email.toLowerCase().includes(search.toLowerCase())
-        );
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        setLoading(true);
+        setError(null);
 
-        if (loading) return <p>Cargando usuarios... ⌛</p>;
+        const data = await getUsers()
+        setusers(data);
+      }
+      catch (error) {
+        setError(error.message);
+      }
+      finally {
+        setLoading(false);
+      }
+
+    };
+    fetchUsers();
+  }, []);
+
+  const handleDeleteuser = (id) => {
+    setusers((prevusers) =>
+      prevusers.filter((user) => user.id !== id)
+    );
+  };
+
+  const filteredUsers = users.filter(user =>
+    user.name.toLowerCase().includes(search.toLowerCase()) ||
+    user.email.toLowerCase().includes(search.toLowerCase())
+  );
+
+  if (loading) return <p>Cargando usuarios... ⌛</p>;
   if (error) return <p>{error}</p>;
 
-    return (
+  return (
     <>
       <h1>Gestión de Usuarios</h1>
 
       <SearchInput
-        value={search}
+        search={search}
         setSearch={setSearch}
       />
+
+      <button onClick={() => setSearch("")}>
+        Limpiar búsqueda
+      </button>
+
+      <p>
+        Mostrando {filteredUsers.length} de {users.length} usuarios
+      </p>
 
       {filteredUsers.length === 0 ? (
         <p>No hay usuarios para mostrar</p>
